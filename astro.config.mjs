40438ea493d79deff1c07294b88c36654f1da062
rhanb <aniel.jeanbaptiste@gmail.com>
2025-03-31 20:10:59 +0200

    first

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://rhanb.com',
+  integrations: [mdx(), sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
